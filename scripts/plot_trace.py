7c2bd38f8b971e89ee354f37573a078c8b0b62ae
Eduardo Aponte <aeduardo@biomed.ee.ethz.ch>
2015-12-12 10:35:25 +0100

    Improved plots.

--- scripts/plot_trace.py
+++ scripts/plot_trace.py
@@ -72,10 +72,12 @@ if __name__ == '__main__':
     ax[0].legend(['$v_t^A$', '$v_t^B$'], loc=0, fontsize=18)
 
     pvS = ax[1].plot(vS, lw=2)
-    ax[1].plot(y, 'o')
+    ax[1].plot(np.abs(1 - y), 'o')
 
     ax[1].legend(['$p(y=A | v^A, v^B)$'], loc=0, fontsize=18)
-
+    ax[1].set_ylim([-0.1, 1.1])
+    ax[1].set_yticks([0, 0.5, 1]) 
+    
     png = png_fname()
     pyl.savefig(png)
 
